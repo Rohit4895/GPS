37814ce701783e30dbf28447b7494ede852c88a9
Rohit <rohit.ramtirth@fidelitservices.com>
2019-02-07 18:21:55 +0530

    attach all files

---
+++ app/src/main/java/com/example/gpspart/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.gpspart;
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    TextView textView;
+    private FusedLocationProviderClient client;
+    Location location1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        requestPermission();
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+
+        button = findViewById(R.id.fetchLocation);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED ) {
+
+                    return;
+                }
+                client.getLastLocation().addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location!=null){
+                            location1 = location;
+                            textView = findViewById(R.id.displayLocation);
+                            textView.setText("Latitude:- "+String.valueOf(location.getLatitude())
+                                    +" Longitude:- "+String.valueOf(location.getLongitude()));
+                        }
+                    }
+                });
+           }
+       });
+
+    }
+
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this,new String[]{ACCESS_FINE_LOCATION},1);
+    }
+}
